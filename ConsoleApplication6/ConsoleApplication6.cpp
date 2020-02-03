2c66d59da30db97061ce51436237e35a9fe5e137
E <minemail@gmail.com>
2020-02-03 20:03:46 +0100

    f

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello Wfforld!\n";
+    std::cout << "HellfWfforld!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -8,7 +8,7 @@ namespace UnitTest1
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
+		//g
 		TEST_METHOD(TestMethod1)
 		{
 		}
